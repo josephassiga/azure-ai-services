d22b445e885d12dd45b432a2dc02b5c25c6bc00f
Joseph Assiga <jassigaa@redhat.com>
2024-05-17 09:48:08 +0200

    use azure ai in a container

---
+++ azure-ai-in-container/openshift/rest-client.sh
@@ -0,0 +1,1 @@
+ curl -X POST "https://language-azure-ai-demo.apps.cluster-b8zv9.dynamic.redhatworkshops.io/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
